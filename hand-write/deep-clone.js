e9723e94a8abfa7f6d0a8de81eef8fe041f13862
AaronKwong929 <aaronkwong929@gmail.com>
2021-08-14 09:52:57 +0800

    111

--- hand-write/deep-clone.js
+++ hand-write/deep-clone.js
@@ -7,9 +7,7 @@ const isObject = source => {
 // while 迭代器 - 性能优化
 const forEach = (list, iterator) => {
   let index = -1;
-  while (++index < list.length) {
-    iterator(list[index]);
-  }
+  while (++index < list.length) iterator(list[index]);
 };
 
 const deepClone = (source, map = new Map()) => {
